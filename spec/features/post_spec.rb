f4dc4ce3ce1564bab3ecda71e374a6bf2b7a38a2
Victor <victorlaucas@gmail.com>
2017-09-21 14:12:05 -0600

    refactored indez page to use post partial

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -20,8 +20,8 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: "Post1")
-      post2 = Post.create(date: Date.today, rationale: "Post2")
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
       visit posts_path
       expect(page).to have_content(/Post1|Post2/)
     end
