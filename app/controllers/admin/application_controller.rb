b11d7124e6ace5042520c34e04a0b3faa3c503db
Victor <victorlaucas@gmail.com>
2017-09-26 11:24:04 -0600

    Implemented tests and page redirection for permission structure

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -5,6 +5,9 @@
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
 module Admin
+  def self.admin_types
+    ['AdminUser']
+  end
 
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_user!
