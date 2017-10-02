094a32233fe176c20b3f894a2f5c0003d4989005
Victor <victorlaucas@gmail.com>
2017-10-02 11:18:01 -0600

    Added phone attr to User and made it required

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-    before do
-      @user = FactoryGirl.create(:user)
-    end
+  before do
+    @user = FactoryGirl.create(:user)
+  end
 
+  describe "creation" do
     it "can be created" do
       expect(@user).to be_valid
     end
 
-    it "cannot be created without first_name, last_name" do
+    it "cannot be created without first_name" do
       @user.first_name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "cannot be created without last_name" do
       @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "cannot be created without phone" do
+      @user.phone = nil
       expect(@user).to_not be_valid
     end
   end
 
   describe "custom name methods" do
-    before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-    end
     it 'has a full name method that combines first and last name' do
       expect(@user.full_name).to eq("SNOW, JON")
     end
