480883fa98f066efbf8e9c90a739c3d576f22dc8
Victor <victorlaucas@gmail.com>
2017-10-03 10:15:11 -0600

    Updated the audit log factory and tested out 6 day default

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,16 @@ AdminUser.create(email: "admin@test.com",
                 last_name: "Name",
                 phone: "5555555555")
 
-puts "Holy Fuck! CRITICAL ERROR!!!! ERASE EVERYTHING!"
+puts "1 Admin User created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
-puts "failing silently"
+puts "100 Posts have been created"
+
+100.times do |audit_log|
+    AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "100 audit logs have been created"
