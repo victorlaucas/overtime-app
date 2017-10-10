895fb06f4a2cfdee2c00430a53f4fa7da814bea7
Victor <victorlaucas@gmail.com>
2017-10-10 13:50:29 -0600

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
-@user = User.create(email: "test@test.com", 
-                    password: "asdfasdf", 
-                    password_confirmation: "asdfasdf", 
-                    first_name: "Jon", 
-                    last_name: "Snow",
-                    phone: "5555555555")
+@user = User.create(email: "test@test.com",
+                                        password: "asdfasdf",
+                                        password_confirmation: "asdfasdf",
+                                        first_name: "Jon",
+                                        last_name: "Snow",
+                                        phone: "5555555555")
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com", 
-                password: "asdfasdf", 
-                password_confirmation: "asdfasdf", 
-                first_name: "Admin", 
-                last_name: "Name",
-                phone: "5555555555")
+AdminUser.create(email: "admin@test.com",
+                                    password: "asdfasdf",
+                                    password_confirmation: "asdfasdf",
+                                    first_name: "Admin",
+                                    last_name: "Name",
+                                    phone: "5555555555")
 
 puts "1 Admin User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
+    Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
