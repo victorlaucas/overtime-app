dbfffdb0aa3f10817c28d253f0d40385dc8a2dcd
Victor <victorlaucas@gmail.com>
2017-10-11 15:18:33 -0600

    Fixed bug in admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destroy] do
+  resources :audit_logs, except: [:new, :edit, :destory] do
     member do
       get :confirm
-    end 
-  end 
+    end
+  end
   
   namespace :admin do
     resources :users
     resources :posts
     resources :admin_users
+    resources :employees
 
     root to: "users#index"
   end
@@ -16,8 +17,9 @@ Rails.application.routes.draw do
   resources :posts do
     member do
       get :approve
-    end 
+    end
   end
+
   devise_for :users, skip: [:registrations]
   root to: 'static#homepage'
-end
+end 
